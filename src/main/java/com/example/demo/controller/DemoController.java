3b4e49edca59b460ac486b7ac76b291972656891
Wilson Fredy Tuco Calizaya <wtucocal@emeal.nttdata.com>
2022-06-23 18:51:59 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public String saludo() {
+        return "Hola mundo";
+    }
+
+}
