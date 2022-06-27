73384f5356aba029cc3a5605120e5b0ea7239953
Fredy <fredtucs7@gmail.com>
2022-06-26 22:18:39 -0500

    update config ci web v3

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -4,13 +4,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/demo")
+@RequestMapping("/")
 public class DemoController {
 
     @GetMapping("/{name}")
     @ResponseStatus(code = HttpStatus.OK)
     public String saludo(@PathVariable String name) {
         return "Hola, bienvenido " + name;
+    }
+
+    @GetMapping
+    @ResponseStatus(code = HttpStatus.OK)
+    public String index() {
+        return "Bienvenido";
     }
 
 }
