baa55956dd05dd666576ebf9b6ce1769dd8ef9e2
Fredy <fredtucs7@gmail.com>
2022-08-16 16:56:44 -0500

    feat: add method reactive mono and flux

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -3,6 +3,9 @@ package com.example.demo.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @RestController
 @RequestMapping("/")
 public class DemoController {
@@ -17,6 +20,18 @@ public class DemoController {
     @ResponseStatus(code = HttpStatus.OK)
     public String index() {
         return "Bienvenido";
+    }
+
+    @GetMapping("/reactive")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Mono<String> reactiveMono() {
+        return Mono.just("Saludo!");
+    }
+
+    @GetMapping("/reactive-flux")
+    @ResponseStatus(code = HttpStatus.OK)
+    public Flux<String> reactiveFlux() {
+        return Flux.just("One", "Two", "Three", "Four");
     }
 
 }
